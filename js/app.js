299d379294ce1df897f4ad2f65f0c0710c080073
V. Kirichenko <fkupme@yandex.ru>
2024-01-09 16:46:48 +0300

    end commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+new Swiper('.slider', {
+	// direction: 'vertical',
+	// freeMode: true,
+	speed: 2400,
+	parallax: {
+		enabled: true,
+		sensetivity: 2.4
+	},
+	spaceBetween: 18,
+	mousewheel: {
+		enabled: true,
+		sensitivity: 2.4
+	}
+})
